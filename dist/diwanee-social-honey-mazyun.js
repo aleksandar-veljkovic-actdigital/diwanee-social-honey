d4ac0285db0e44b186171ecbdb02a135a3ef7eee
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2016-09-20 11:15:22 +0200

    app is working

--- dist/diwanee-social-honey-mazyun.js
+++ dist/diwanee-social-honey-mazyun.js
@@ -3,7 +3,7 @@ this["DiwaneeSocialHoney"]["templates"] = this["DiwaneeSocialHoney"]["templates"
 this["DiwaneeSocialHoney"]["templates"]["diwanee-social-honey"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "      <li class=\"ui-list__item\">\n\n\n\n\n\n        <article class=\"media-small related-small\">\n          <div class=\"media-small__img\">\n            <a href=\""
+  return "      <li class=\"ui-list__item\">\n\n        <article class=\"media-small related-small\">\n          <div class=\"media-small__img\">\n            <a href=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
     + "\" class=\"hrl\">\n              <img src='"
     + alias4((helpers.thumborThumb || (depth0 && depth0.thumborThumb) || alias2).call(alias1,((stack1 = (depth0 != null ? depth0.thumbnailImageURLs : depth0)) != null ? stack1["0"] : stack1),{"name":"thumborThumb","hash":{},"data":data}))
@@ -11,7 +11,7 @@ this["DiwaneeSocialHoney"]["templates"]["diwanee-social-honey"] = Handlebars.tem
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "' />\n            </a>\n          </div>\n          <div class=\"media-small__body\">\n            <h3 class=\"media-small__title remove-margin\">\n              "
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "\n            </h3>\n          </div>\n        </article>\n\n\n\n\n\n\n\n      </li>\n";
+    + "\n            </h3>\n          </div>\n        </article>\n\n      </li>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,16 @@ var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var debug = require('gulp-debug');
+var print = require('gulp-print');
 
 var template = function (publication) {
-  gulp.src('src/mazyun/*.handlebars')
+  gulp.src('src/' + publication + '/*.handlebars')
+          .pipe(plumber())
+          .pipe(print(function (filepath) {
+            return "precompiling: " + filepath
+          }))
           .pipe(handlebars({
             handlebars: require('handlebars')
           }))
@@ -14,10 +21,23 @@ var template = function (publication) {
             namespace: 'DiwaneeSocialHoney.templates',
             noRedeclare: true // Avoid duplicate declarations 
           }))
-          .pipe(concat('diwanee-social-honey-mazyun.js'))
+          .pipe(concat('diwanee-social-honey-' + publication + '.js'))
           .pipe(gulp.dest('dist'));
 };
 
-gulp.task('default', function () {
-  template();
+// Add publications hire
+var templates = function () {
+  template('mazyun');
+};
+
+gulp.task('watch', function () {
+  return gulp.watch('src/**/*.handlebars', function () {
+    templates();
+  });
 });
+
+gulp.task('default', function () {
+  templates();
+  gulp.start('watch');
+});
+
